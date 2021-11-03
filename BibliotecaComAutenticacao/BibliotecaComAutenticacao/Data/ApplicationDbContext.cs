23a320b2fd15ba063347f6f44c3bdd77b409660c
Bianca Antunes <62080345+biancaantunes@users.noreply.github.com>
2021-11-03 09:55:15 -0300

    Biblioteca

---
+++ BibliotecaComAutenticacao/BibliotecaComAutenticacao/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BibliotecaComAutenticacao.Models;
+
+namespace BibliotecaComAutenticacao.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BibliotecaComAutenticacao.Models.Autor> Autor { get; set; }
+        public DbSet<BibliotecaComAutenticacao.Models.Cliente> Cliente { get; set; }
+        public DbSet<BibliotecaComAutenticacao.Models.Emprestimo> Emprestimo { get; set; }
+        public DbSet<BibliotecaComAutenticacao.Models.Livro> Livro { get; set; }
+        public DbSet<BibliotecaComAutenticacao.Models.Status> Status { get; set; }
+    }
+}
